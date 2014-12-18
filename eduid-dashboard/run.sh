d9cc79d06c45369fd87a798ff5d197d9d6706eb2
Fredrik Thulin <fredrik@thulin.net>
2014-12-18 16:49:41 +0100

    add idp and dashboard, and a lot of fixes

---
+++ eduid-dashboard/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# To debug your container:
+#
+#   DOCKERARGS="--entrypoint /bin/bash -i -t" bash -x ./run.sh
+#
+
+name="dashboard"
+
+if [ $(id -u) -ne 0 ]; then
+    sudo="sudo"
+fi
+
+mkdir -p run log etc
+
+srcdir=$(echo ~/work/NORDUnet/eduid-${name})
+if [ -d "${srcdir}" ]; then
+    # map developers local source copy into /opt/eduid/src and set PYTHONPATH accordingly
+    src_volume="-v ${srcdir}:/opt/eduid/src --env=PYTHONPATH=/opt/eduid/src"
+fi
+
+if $sudo docker ps | awk '{print $NF}' | grep -qx $name; then
+    echo "$0: Docker container with name $name already running. Press enter to restart it, or ctrl+c to abort."
+    read foo
+    $sudo docker kill $name
+fi
+
+$sudo docker rm $name
+$sudo docker run --rm=true \
+    --name ${name} \
+    --hostname ${name} \
+    --dns=172.17.42.1 \
+    -v $PWD/etc:/opt/eduid/etc \
+    -v $PWD/run:/opt/eduid/run \
+    -v $PWD/log:/var/log/eduid \
+    $src_volume \
+    $DOCKERARGS \
+    docker.sunet.se/eduid/eduid-${name}
---
+++ eduid-idp/etc/idp_conf.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+from saml2 import BINDING_HTTP_REDIRECT
+from saml2 import BINDING_HTTP_POST
+from saml2 import BINDING_SOAP
+from saml2.saml import NAME_FORMAT_URI
+from saml2.saml import NAMEID_FORMAT_TRANSIENT
+from saml2.saml import NAMEID_FORMAT_PERSISTENT
+
+BASE = 'http://idp.:8080'
+
+CONFIG = {
+    "entityid": "%s/idp.xml" % BASE,
+    "description": "eduID LOCAL TEST identity provider",
+    "service": {
+        "idp": {
+            "name": "eduID LOCAL TEST IdP",
+            "endpoints": {
+                "single_sign_on_service": [
+                    ("%s/sso/redirect" % BASE, BINDING_HTTP_REDIRECT),
+                    ("%s/sso/post" % BASE, BINDING_HTTP_POST),
+                ],
+                "single_logout_service": [
+                    ("%s/slo/soap" % BASE, BINDING_SOAP),
+                    ("%s/slo/post" % BASE, BINDING_HTTP_POST),
+                    ("%s/slo/redirect" % BASE, BINDING_HTTP_REDIRECT)
+                ],
+            },
+            "policy": {
+                "default": {
+                    "lifetime": {"minutes": 5},
+                    "attribute_restrictions": None, # means all I have
+                    "name_form": NAME_FORMAT_URI,
+                    "nameid_format": NAMEID_FORMAT_PERSISTENT,
+                    "entity_categories": ["swamid", "edugain"],
+                    "fail_on_missing_requested": False,  # Don't fail on unsatisfied RequiredAttributes
+                },
+            },
+            "subject_data": ("mongodb", "mongodb://eduid_idp:eduid_idp_pw@mongodb./eduid_idp_pysaml2"),
+            "session_storage": ("mongodb", "mongodb://eduid_idp:eduid_idp_pw@mongodb./eduid_idp_pysaml2"),
+            "name_id_format": [NAMEID_FORMAT_TRANSIENT,
+                               NAMEID_FORMAT_PERSISTENT],
+
+            "scope": ["local.eduid.se"],
+
+        },
+    },
+    "debug": 1,
+    "key_file": "/opt/eduid/etc/idp-public-snakeoil.key",
+    "cert_file": "/opt/eduid/etc/idp-public-snakeoil.pem",
+    "metadata": {
+        "local": ["/opt/eduid/etc/dashboard_metadata.xml",
+                  ],
+    },
+    "organization": {
+        "display_name": "eduID LOCAL TEST",
+        "name": "eduID LOCAL TEST",
+        "url": "http://www./",
+    },
+    "contact_person": [
+        {
+            "contact_type": "technical",
+            "given_name": "eduID",
+            "sur_name": "developers",
+            "email_address": "eduid-dev@SEGATE.SUNET.SE"
+        }, {
+            "contact_type": "support",
+            "given_name": "Support",
+            "email_address": "support@eduid.se"
+        },
+    ],
+
+    #"crypto_backend": "XMLSecurity",
+
+    #"xmlsec_binary": "/bin/false",  # pysaml2 checks for this
+    # This database holds the map between a subjects local identifier and
+    # the identifier returned to a SP
+    "attribute_map_dir": "/opt/eduid/etc/attributemaps",
+    "logger": {
+        "rotating": {
+            "filename": "/var/log/eduid/idp.log",
+            "maxBytes": 500000,
+            "backupCount": 5,
+        },
+        "loglevel": "debug",
+    }
+}
