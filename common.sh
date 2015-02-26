413ad958b4d2eda8f082daec62552edc4668b257
Fredrik Thulin <fredrik@thulin.net>
2015-02-26 15:16:48 +0100

    move code to figure out developers source path into common.sh

--- common.sh
+++ common.sh
@@ -1,10 +1,32 @@
-
-DEFAULT="~/work/NORDUnet"
-
+#
+# Return (echo) the base path to developers source repositorys
 function get_code_path {
-  if [[ "$EDUID_SRC_PATH" ]]; then
-    echo "$EDUID_SRC_PATH"
-  else
-    echo "$DEFAULT"
-  fi
+  EDUID_SRC_PATH=${EDUID_SRC_PATH-'~/work/NORDUnet'}
+  echo "`eval echo ${EDUID_SRC_PATH//>}`"  # handle '>' in EDUID_SRC_PATH
+}
+
+#
+# Figure out suitable 'docker run' parameters to volume-mount in any present
+# source code for this container, and an according PYTHONPATH to the volumes
+function get_developer_params {
+    host_src_path="$(echo $(get_code_path))"
+    guest_src_path="/opt/eduid/src"
+
+    src_volumes=""
+    pp=""
+
+    for name in $*; do
+	srcdir="${host_src_path}/${name}"
+	if [ -d "${srcdir}" ]; then
+            # map developers local source copy into /opt/eduid/src and set PYTHONPATH accordingly
+	    src_volumes+=" -v ${srcdir}:${guest_src_path}/${name}"
+	    if [ "x${pp}" = "x" ]; then
+		pp="--env=PYTHONPATH=${guest_src_path}/${name}"
+	    else
+		pp+=":${guest_src_path}/${name}"
+	    fi
+	fi
+    done
+
+    echo "${src_volumes} ${pp}"
 }
--- eduid-am/run.sh
+++ eduid-am/run.sh
@@ -15,29 +15,8 @@ fi
 
 mkdir -p log etc src
 
-host_src_path="$(echo $(get_code_path))"
-guest_src_path="/opt/eduid/src"
-
-srcdir="${host_src_path}/${name}"
-if [ -d "${srcdir}" ]; then
-    # map developers local source copy into /opt/eduid/src and set PYTHONPATH accordingly
-    src_volume="-v ${srcdir}:${guest_src_path}/${name}"
-    env_vars="--env=PYTHONPATH=${guest_src_path}/${name}"
-
-    dashboard_amp_dir="${host_src_path}/eduid-dashboard-amp"
-    if [ -d "${dashboard_amp_dir}" ]; then
-        src_volume+=" -v ${dashboard_amp_dir}:${guest_src_path}/eduid-dashboard-amp"
-        env_vars+=":${guest_src_path}/eduid-dashboard-amp"
-    fi
-    
-    signup_amp_dir="${host_src_path}/eduid-signup-amp"
-    if [ -d "${signup_amp_dir}" ]; then
-        src_volume+=" -v ${signup_amp_dir}:${guest_src_path}/eduid-signup-amp"
-        env_vars+=":${guest_src_path}/eduid-signup-amp"
-    fi
-  
-    src_volume+=" ${env_vars}"
-fi
+src_params="$(get_developer_params $name eduid-signup-amp eduid-dashboard-amp)"
+echo "Source parameters: ${src_params}"
 
 if $sudo docker ps | awk '{print $NF}' | grep -qx $name; then
     echo "$0: Docker container with name $name already running. Press enter to restart it, or ctrl+c to abort."
@@ -52,6 +31,6 @@ $sudo docker run --rm=true \
     --dns=172.17.42.1 \
     -v $PWD/etc:/opt/eduid/etc \
     -v $PWD/log:/var/log/eduid \
-    $src_volume \
+    $src_params \
     $DOCKERARGS \
     docker.sunet.se/eduid/${name}
