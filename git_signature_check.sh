a5e7b3f6a4c20825c1a42c4bc57aa8e3a5f1d7d9
John Van de Meulebrouck Brendgard <john@nordu.net>
2016-01-30 20:35:08 +0100

    Added script to verify commit signatures

---
+++ git_signature_check.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# This script verifies all the signatures of commits in the
+# current branch, or all commits made after the last trusted tag.
+#
+# Inspired by https://mikegerwitz.com/papers/git-horror-story.html
+# and how Cosmos verifies git tags.
+
+t="$( echo $'\t' )"
+
+echo "Doing a git pull ..."
+git pull
+
+last_tag="$(git describe --tags --abbrev=0 2>/dev/null)"
+
+if [ ${?} -ne 0 ]; then
+    echo "No tags found, verifying all commits instead."
+    git_log="$(git log --pretty="format:%H${t}%aN${t}%s${t}%G?" --first-parent \
+        | grep -v "${t}G$")"
+else
+    echo "Verifying last tag: ${last_tag} and the commits after that"
+
+    git tag -v "${last_tag}" | grep ^gpg:
+    # again to not mask exit status of git with grep
+    git tag -v ${last_tag} > /dev/null 2>&1
+
+    if [ ${?} -ne 0 ]; then
+        echo "WARNING: The last tag was not signed by a trusted key!, aborting."
+        exit 1
+    fi
+
+    tag_object="$(git tag -v ${last_tag} 2>&1 | grep ^object | cut -d' ' -f2)"
+
+    revision_range="${tag_object}..HEAD"
+   
+    git_log="$(git log --pretty="format:%H${t}%aN${t}%s${t}%G?" \
+        ${revision_range} --first-parent | grep -v "${t}G$")"
+fi
+
+if [ ! -z "${git_log}" ]; then
+    echo -e "\n----WARNING: unsigned or untrusted commits----"
+    echo "${git_log}"
+    exit 1
+else
+    echo -e "\n----All commits and/or tags verified OK----"
+    exit 0
+fi
