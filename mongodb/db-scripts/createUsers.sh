ff8ada023d11f2526b26cb4aa5e36c861ecb33ea
Johan Lundberg <lundberg@nordu.net>
2016-04-07 15:08:48 +0200

    Added oidc_proofing mongo user

--- mongodb/db-scripts/createUsers.sh
+++ mongodb/db-scripts/createUsers.sh
@@ -187,6 +187,27 @@ for db in eduid_am; do
 '
 done
 
+# User eduid_actions, read
+#
+for db in eduid_am; do
+    mongo localhost/${db} --eval '
+      if (db.system.users.count({"user": "eduid_actions"}) == 0) {
+         db.addUser( { user: "eduid_actions", pwd: "eduid_actions_pw", roles: ["read"] } );
+      }
+'
+done
+
+#
+# User eduid_oidc_proofing, read
+#
+for db in eduid_am; do
+    mongo localhost/${db} --eval '
+      if (db.system.users.count({"user": "eduid_oidc_proofing,"}) == 0) {
+         db.addUser( { user: "eduid_oidc_proofing", pwd: "eduid_oidc_proofing_pw", roles: ["read"] } );
+      }
+'
+done
+
 #
 # User eduid_idproofing_letter, readWrite
 #
@@ -198,4 +219,14 @@ for db in eduid_idproofing_letter; do
 '
 done
 
+#
+# User eduid_oidc_proofing, readWrite
+#
+for db in eduid_oidc_proofing; do
+    mongo localhost/${db} --eval '
+      if (db.system.users.count({"user": "eduid_oidc_proofing"}) == 0) {
+         db.addUser( { user: "eduid_oidc_proofing", pwd: "eduid_oidc_proofing_pw", roles: ["readWrite"] } );
+      }
+'
+done
 
