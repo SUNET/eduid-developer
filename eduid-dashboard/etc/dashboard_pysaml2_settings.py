15434bcaf547c81dc2dc3b12aeb3d16f39b0570a
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-08-02 11:38:42 +0200

    fixing some urls

--- eduid-dashboard/etc/dashboard_pysaml2_settings.py
+++ eduid-dashboard/etc/dashboard_pysaml2_settings.py
@@ -8,7 +8,7 @@ from saml2 import attributemaps
 
 DEFAULT_ATTRIBUTEMAPS = path.dirname(attributemaps.__file__)
 
-BASE_URL = 'http://dashboard.eduid.local.emergya.info:8080/'
+BASE_URL = 'https://dashboard.eduid.local.emergya.info/'
 SAML2DIR = path.dirname(__file__)
 
 SAML_CONFIG = {
@@ -56,10 +56,10 @@ SAML_CONFIG = {
                 # the keys of this dictionary are entity ids
                 'http://idp.eduid.local.emergya.info:8080/idp.xml': {
                     'single_sign_on_service': {
-                        saml2.BINDING_HTTP_REDIRECT: 'http://idp.eduid.local.emergya.info:8080/sso/redirect',
+                        saml2.BINDING_HTTP_REDIRECT: 'https://idp.eduid.local.emergya.info/sso/redirect',
                     },
                     'single_logout_service': {
-                        saml2.BINDING_HTTP_REDIRECT: 'http://idp.eduid.local.emergya.info:8080/slo/redirect',
+                        saml2.BINDING_HTTP_REDIRECT: 'https://idp.eduid.local.emergya.info/slo/redirect',
                     },
                 },
             },
