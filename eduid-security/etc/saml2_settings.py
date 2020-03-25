50c354ebb08d013e05dbb69666aee120787c31f2
enriquepablo <enrique@cazalla.net>
2020-03-25 13:37:37 +0100

    eperez https env

--- eduid-security/etc/saml2_settings.py
+++ eduid-security/etc/saml2_settings.py
@@ -8,7 +8,7 @@ from saml2 import attributemaps
 
 DEFAULT_ATTRIBUTEMAPS = path.dirname(attributemaps.__file__)
 
-BASE_URL = 'http://dashboard.eduid.docker:8080/services/authn'
+BASE_URL = 'https://dashboard.eduid.local.emergya.info/services/authn'
 SAML2DIR = path.dirname(__file__)
 
 SAML_CONFIG = {
@@ -55,12 +55,12 @@ SAML_CONFIG = {
                 # present in our metadata
 
                 # the keys of this dictionary are entity ids
-                'http://idp.eduid.docker:8080/idp.xml': {
+                'https://idp.eduid.local.emergya.info/idp.xml': {
                     'single_sign_on_service': {
-                        saml2.BINDING_HTTP_REDIRECT: 'http://idp.eduid.docker:8080/sso/redirect',
+                        saml2.BINDING_HTTP_REDIRECT: 'https://idp.eduid.local.emergya.info/sso/redirect',
                     },
                     'single_logout_service': {
-                        saml2.BINDING_HTTP_REDIRECT: 'http://idp.eduid.docker:8080/slo/redirect',
+                        saml2.BINDING_HTTP_REDIRECT: 'https://idp.eduid.local.emergya.info/slo/redirect',
                     },
                 },
             },
