5f8b56a6f11e30a42a08535c1364b24d1ae47044
Johan Lundberg <lundberg@sunet.se>
2020-06-02 09:45:29 +0200

    Create symlinks if missing

--- vagrant/maintenance.sh
+++ vagrant/maintenance.sh
@@ -13,3 +13,7 @@ apt-get clean
 # Fix networking
 ip addr del 172.16.10.10/24 dev enp0s8
 brctl addif br-eduid enp0s8
+
+# Link to sources if missing
+ln -svf /opt/src/eduid-front /opt/eduid-developer/sources/
+ln -svf /opt/src/eduid-html /opt/eduid-developer/sources/
