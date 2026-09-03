1086894463bbe17f2062e365f5e55065b3105127
Johan Lundberg <lundberg@sunet.se>
2026-09-03 15:32:33 +0200

    samleid.eduid.docker -> eid.eduid.docker

--- eduid-samleid/etc/saml2_settings.py
+++ eduid-samleid/etc/saml2_settings.py
@@ -7,7 +7,7 @@ from saml2 import attributemaps
 
 DEFAULT_ATTRIBUTEMAPS = path.dirname(attributemaps.__file__)
 
-BASE_URL = "https://samleid.eduid.docker"
+BASE_URL = "https://eid.eduid.docker"
 SAML2DIR = path.dirname(__file__)
 
 SAML_CONFIG = {
